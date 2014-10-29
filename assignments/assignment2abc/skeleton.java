88cc494cc0302a7516c8a526dadf0e5dd236b74e
Kaung Htet Zaw <emoosx@gmail.com>
2014-10-30 03:35:44 +0800

    Skeleton 2a input

--- assignments/assignment2abc/skeleton.java
+++ assignments/assignment2abc/skeleton.java
@@ -6,10 +6,34 @@ class skeleton {
   public static void main(String[] args) {
     IntegerScanner scanner = new IntegerScanner(System.in);
     PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+    int T, C, N, K;
+
+    T = scanner.nextInt();         // number of test cases
+    for(int t = 0; t < T; t++) {
+        C = scanner.nextInt();     // number of clauses
+        N = scanner.nextInt();     // number of variables
+        for(int c = 0; c < C; c++) {
+          // the first integer K describes the number of literals in that clause
+          K = scanner.nextInt();
+          // the next K integers describe the literals.
+          int literals[] = new int[K];
+          for(int k = 0; k < K; k++) {
+            literals[k] = scanner.nextInt();
+          }
+
+          convert(literals);
+
+          
+          // i = literal x(i) if i > 0 or literal -x(-i) if i < 0;
+          // e.g., (x1 v x3 v -x4) will be = 3 1 3 -4;
+        }
+    }
   }
   
 
-  private static void woohoo() {
+  private static void convert(int[] literals) {
+    System.out.println("converting");
   }
 }
 
